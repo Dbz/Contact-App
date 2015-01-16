3d10d89ac367fbfd00cecf83194e3366540f94d3
Daniel <BurtDaniel@Gmail.com>
2015-01-15 19:02:10 -0800

    finished controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,31 @@
 class ContactsController < ApplicationController
   def index
+    @contact = Contact.new(flash[:contact_params]) || Contact.new
+    @contacts = Contact.all
     render :index
+  end
+  
+  def create
+    @contacts = Contact.all
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      @contact = Contact.new
+      redirect_to root_url
+    else
+      flash[:errors] = @contact.errors.full_messages
+      flash[:contact_params] = contact_params
+      redirect_to root_url
+    end
+  end
+  
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    redirect_to root_url
+  end
+  
+  private
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :phone_number, :image)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Contact.where(
+  first_name: "Daniel",
+  last_name: "Burt",
+  phone_number: "654-563-4302",
+  image: "http://dbz.rocks/include/img/profile/me.jpg"
+).first_or_create!
+Contact.where(
+  first_name: "Ned",
+  last_name: "Stark",
+  phone_number: "654-563-4102",
+  image: "http://i.dailymail.co.uk/i/pix/2014/05/10/article-2625187-1DBA0F6200000578-443_306x423.jpg"
+).first_or_create!
+Contact.where(
+  first_name: "Robb",
+  last_name: "Stark",
+  phone_number: "654-563-4372",
+  image: "http://vignette3.wikia.nocookie.net/vampirediaries/images/e/e4/Robb_Stark.jpg/revision/latest?cb=20140411010003"
+).first_or_create!
+Contact.where(
+  first_name: "Arya",
+  last_name: "Stark",
+  phone_number: "654-563-4304",
+  image: "http://flavorwire.files.wordpress.com/2013/07/screen-shot-2013-07-01-at-5-27-11-pm.png"
+).first_or_create!
+Contact.where(
+  first_name: "Little",
+  last_name: "Finger",
+  phone_number: "654-563-4309",
+  image: "http://i.imgur.com/6JHgc18.gif"
+).first_or_create!
