483b70401b66d781e86836c828e467f3de6ac985
Daniel <BurtDaniel@Gmail.com>
2015-01-16 00:06:10 -0800

    added model layer validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < ActiveRecord::Base
   validates :first_name, :last_name, :phone_number, presence: true
+  validates :phone_number, uniqueness: { scope: [:first_name, :last_name] }
   before_save :add_default_image
   
   def add_default_image
